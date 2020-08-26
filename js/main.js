c4bbd85a9c3dad5394b7bdd82cf4fe5b180d3503
jtipt <jtipt.rail@gmail.com>
2020-08-26 20:34:51 +0530

    finilize style

--- js/main.js
+++ js/main.js
@@ -22,11 +22,10 @@ function getMovies(searchText) {
          // Everything you see after search is here
          $.each(movies, function (index, movie) {
             output += `
-            <div class="col-md-3">
+            <div class="col-xs-6 col-md-4 col-lg-3">
                <div class="text-center">
-                  <img src="https://image.tmdb.org/t/p/w342${movie.poster_path}">
-                  <h5>${movie.title}</h5>
-                  <a onclick="movieSelected('${movie.id}')" class="btn btn-dark" href="#">Movie Details</a>
+                  <img src="https://image.tmdb.org/t/p/w342${movie.poster_path}" alt="404 Not Found">
+                  <a onclick="movieSelected('${movie.id}')" href="#"><h5>${movie.title}</h5></a>
                </div> 
             </div>
             `;
@@ -70,33 +69,43 @@ function getMovie() {
             <img src="https://image.tmdb.org/t/p/w342${movie.poster_path}" class="">
             </div>
             <div class = "col-md-8">
-               <h2 class="movieHead">${movie.title}</h2>
-               <ul class="list-group">
-                  <li class="list-group-item"><strong>Genre: </strong> ${movie.genres[0].name}, ${movie.genres[1].name}, ${movie.genres[2].name}</li>
-                  <li class="list-group-item"><strong>Released: </strong>${movie.release_date}</li>
-                  <li class="list-group-item"><strong>Runtime (mins): </strong>${movie.runtime}</li>
-                  <li class="list-group-item"><strong>Rating: </strong>${movie.vote_average}</li>
-                </ul>
+            <a href="http://imdb.com/title/${movie.imdb_id}/"><h2 class="movieHead">${movie.title}</h2></a>
+               <ul class="tilesWrap">
+                  <li class="l">
+                     <h2>Details</h2>
+                     <h3>Details</h3>
+                        <ul>
+                           <li class=""><strong>Genre: </strong> ${movie.genres[0].name}, ${movie.genres[1].name}, ${movie.genres[2].name}</li>
+                           <li class=""><strong>Released: </strong>${movie.release_date}</li>
+                           <li class=""><strong>Runtime (mins): </strong>${movie.runtime}</li>
+                           <li class=""><strong>Rating: </strong>${movie.vote_average}</li>
+                     </ul>
+                  </li>
+               </ul>
+              
             </div>
          </div>
          <div class = "row">
-            <div class = "well">
+               <ul class="tilesWrap">
+               <li class = "l l2">
+               <h2>Plot</h2>
                <h3>Plot</h3>
-               ${movie.overview}
+               <p style="font-size:2rem">${movie.overview}</p>
                <hr>
-               <a href="http://imdb.com/title/${movie.imdb_id}" class="btn btn-dark">View on IMDB</a>
-               <a href="index.html" class="btn btn-dark">Go back</a>
-               <br>
-               <br>           
-            </div>
+               
+                     
+               </li>
+            <ul>
          </div>
          <div class = "row">
-            <div class = "well">
-               <h3>Cast</h3>
+            <ul class="tilesWrap">
+            <li class="l l2">   
+            <h3>Cast</h3>
+            <h2>Cast</h2>
                
              ${actors}
-
-            </div>
+            </li>
+            </ul>
          </div>
          `;
 
